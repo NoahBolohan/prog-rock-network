f25ee337e130fc85b0c19b4551009969f7fd6c71
Noah Bolohan <noah.bolohan@gmail.com>
2024-05-31 05:34:57 -0400

    Parsing for non-member pages

--- parse_html_sources.py
+++ parse_html_sources.py
@@ -1,13 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
+from datetime import datetime
 
+def split_date_string(date_string):
+    if len(ds_split := date_string.split('–')):
+        try:
+            start = datetime.strptime(ds_split[0], '%B %Y')
+        except ValueError:
+            start = datetime.strptime(ds_split[0], '%Y')
+
+        try:
+            end = datetime.strptime(ds_split[1], '%B %Y')
+        except ValueError:
+            end = datetime.strptime(ds_split[1], '%Y')
+    else:
+        try:
+            start = datetime.strptime(date_string, '%B %Y')
+        except ValueError:
+            start = datetime.strptime(date_string, '%Y')
+
+        end = start
+    
+    return start, end
+
+def range_in_range(a, b, x, y):
+
+    start_a = min(a,b)
+    end_a = max(a,b)
+
+    start_c = min(x,y)
+    end_c = max(x,y)
+
+    return (a<=x and y<b) or (a<x and y<=b) or (a<x and y<b)
+    
 def td_specs(td_list,idx):
     values = [
-        s.get_text().strip() for s in td_list[idx].find_all('li')
+        re.sub(r"[\[].*?[\]]", "", s.get_text().strip()) for s in td_list[idx].find_all('li')
     ] if len(
         td_list[idx].find_all('li')
-    ) > 0 else td_list[idx].get_text().strip()
+    ) > 0 else re.sub(r"[\[].*?[\]]", "", td_list[idx].get_text().strip())
 
     if td_list[idx].has_attr("rowspan"):
         skip = int(td_list[idx]["rowspan"]) - 1
@@ -16,23 +49,7 @@ def td_specs(td_list,idx):
 
     return values, skip
 
-with open("./bs4_input.json") as f:
-    data = json.load(f)
-
-bands = {}
-
-# Iterate over BS4 band configs in `bs4_input.json`
-for band, info in data.items():
-
-    bands[band] = {
-        "title" : info["title"],
-        "members" : {}
-    }
-    
-    page = requests.get(info["members_url"])
-
-    soup = BeautifulSoup(page.content, "html.parser")
-
+def parse_member_page(soup, info):
     members_dict = {}
     
     # Iterate over member type
@@ -78,8 +95,63 @@ for band, info in data.items():
             members_dict[name]["instruments"] = instrument_values
             members_dict[name]["member type"] = info["spans"][span]
 
+    return members_dict
 
-    bands[band]["members"] = members_dict
+def parse_main_page(soup, info):
+    members_dict = {}
+
+    # Iterate over member type
+    for span in info["spans"].keys():
+
+        from_members = soup.select(f"span#{span}")[0]
+        member_list = from_members.find_next("ul").find_all('li')
+
+        for member in member_list:
+            name = member.get_text().split(' – ')[0]
+            details = re.sub(r"[\[].*?[\]]", "", '–'.join(member.get_text().split(' – ')[1:]))
+
+            year_values = [s.strip("()").split(',') for s in re.findall(r"\(.*?\)", details)]
+            year_values = [s.strip() for list_of_s in year_values for s in list_of_s if s.strip() != '']
+
+            for year_value_checked in year_values:
+                for year_value_against in year_values:
+                    if range_in_range(*split_date_string(year_value_against), *split_date_string(year_value_checked)):
+                        year_values.remove(year_value_checked)
+
+
+            instrument_values = [s.split(', ') for s in re.findall(r'(.*?)\(.*?\)', details)]
+            instrument_values = [s.strip() for list_of_s in instrument_values for s in list_of_s if s.strip() != '']
+
+            members_dict[name] = {
+                "years_active" : year_values,
+                "instruments" : instrument_values,
+                "member type" : info["spans"][span]
+            }
+            
+    return members_dict
+
+with open("./bs4_input.json") as f:
+    data = json.load(f)
+
+bands = {}
+
+# Iterate over BS4 band configs in `bs4_input.json`
+for band, info in data.items():
+
+    bands[band] = {
+        "title" : info["title"],
+        "members" : {}
+    }
+    
+    page = requests.get(info["members_url"])
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    if info["members_page"] == 'y':
+        # bands[band]["members"] = parse_member_page(soup, info)
+        continue
+    else:
+        bands[band]["members"] = parse_main_page(soup, info)
 
 with open("./input.json", "w",  encoding="utf8") as f:
     json.dump(bands , f, ensure_ascii=False) 
